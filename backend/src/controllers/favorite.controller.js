2dcf4a190637068fa19e0a0ab3310cc9671b37fc
“Florence <162617498+TheTable1@users.noreply.github.com>
2025-06-09 17:53:54 +0700

    update menu favorite

--- backend/src/controllers/favorite.controller.js
+++ backend/src/controllers/favorite.controller.js
@@ -3,26 +3,24 @@ const {
   error,
   notFound,
   created,
-} = require("../utils/response.utils");
+} = require('../utils/response.utils');
+
 const {
   addFavBookSchema,
   favBookIdParamSchema,
-} = require("../validations/favorite.validation");
-const validateZod = require("../validations/validateZod");
-
-const favBookService = require("../services/favorite.service");
+} = require('../validations/favorite.validation');
+const validateZod = require('../validations/validateZod');
+const favBookService = require('../services/favorite.service');
 
 const getFavById = {
-  description: "Get all lists",
+  description: "Get user's favorite books",
   tags: ["api", "favorite"],
-  auth: {
-    strategy: "jwt",
-  },
+  auth: { strategy: "jwt" },
   handler: async (request, h) => {
     try {
       const userId = Number(request.auth.credentials.id);
       const data = await favBookService.getFavById(userId);
-      return success(h, data, "Fetched Favorite Book");
+      return success(h, data, "Fetched Favorite Books");
     } catch (err) {
       console.error(err);
       return error(h, err.message);
@@ -31,20 +29,18 @@ const getFavById = {
 };
 
 const addFavBook = {
-  description: "Get all lists",
+  description: "Add a favorite book",
   tags: ["api", "favorite"],
-  auth: {
-    strategy: "jwt",
-  },
+  auth: { strategy: "jwt" },
   validate: {
     payload: validateZod(addFavBookSchema),
   },
   handler: async (request, h) => {
     try {
-      const userId = request.auth.credentials.id;
+      const userId = Number(request.auth.credentials.id);
       const { bookId } = request.payload;
       const fav = await favBookService.createFavBook(userId, bookId);
-      return success(h, fav, "Add Favorite Book");
+      return created(h, fav, "Added Favorite Book");
     } catch (err) {
       console.error(err);
       return error(h, err.message);
@@ -53,28 +49,25 @@ const addFavBook = {
 };
 
 const deleteFavBook = {
-  description: "Get all lists",
+  description: "Remove a favorite book",
   tags: ["api", "favorite"],
-  auth: {
-    strategy: "jwt",
-  },
+  auth: { strategy: "jwt" },
   validate: {
     params: validateZod(favBookIdParamSchema),
   },
   handler: async (request, h) => {
     try {
-      const userId = request.auth.credentials.id;
-      const favBookId = request.params.id;
+      const userId = Number(request.auth.credentials.id);
+      const favBookId = Number(request.params.id);
       const result = await favBookService.deleteFavBook(userId, favBookId);
       if (!result) {
         return notFound(h, null, "Favorite not found");
       }
-      return success(h, result, "Delete Favorite Book");
+      return success(h, result, "Deleted Favorite Book");
     } catch (err) {
       console.error(err);
       return error(h, err.message);
     }
-
   },
 };
 
